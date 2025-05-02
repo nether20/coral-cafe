4cd68caa9d11963b42cc17a15716dd7a6293c6b4
nether20 <345268@melissaisd.org>
2025-05-02 15:33:51 +0000

    Pending changes exported from your codespace

---
+++ src/app/menu/page.js
@@ -0,0 +1,46 @@
+export default function Menu() {
+    return (
+        <div className="menu">
+            <h2 className="back"><a href="/">Back</a></h2>
+            <h1>Menu</h1>
+            <h2>Drinks</h2>
+            <li>
+                <ul>$5 - Krabby Kocktail</ul>
+                <ul><img src="https://cdn.mos.cms.futurecdn.net/SRdGNTqjvjVoQ2XYrgEjLN-1200-80.jpg"/></ul>
+                <ul>$5 - Sea Cucmber Slushie</ul>
+                <ul><img src="https://www.nhm.ac.uk/content/dam/nhm-www/discover/sea-cucumbers/leopard-sea-cucumber-cuverian-tubules-defence-full-width.jpg.thumb.1160.1160.png"/></ul>
+                <ul>$7 - Seagrass Iced Tea</ul>
+                <ul><img src="https://floridadep.gov/sites/default/files/media-folders/media-root/St.%20Martins%20Marsh%20Aquatic%20Preserve%20-%20DEP%20Staff%20-%20Jon%20Brucker%20-%20Turtlegras%20Medow.jpg"/></ul>
+                <ul>$5 - Snail-Cola</ul>
+                <ul><img src="https://columbian.gwu.edu/sites/g/files/zaxdzs4901/files/image/Nautilus%20Pic_1080x720.jpg"/></ul>
+            </li>
+            <br/>
+            <h2>Appetizers</h2>
+            <li>
+                <ul>$10 - Parrotfish in a Blanket</ul>
+                <ul><img src="https://oceanconservancy.org/wp-content/uploads/2021/06/Screen-Shot-2021-06-17-at-11.31.27-AM-e1623943971271.png"/></ul>
+                <ul>$8 - Barnacle Fries</ul>
+                <ul><img src="https://theoutershores.com/wp-content/uploads/2013/12/p1010577_lr.jpg?w=1024"/></ul>
+                <ul>$15 - Snailcuterie</ul>
+                <ul><img src="https://columbian.gwu.edu/sites/g/files/zaxdzs4901/files/image/Nautilus%20Pic_1080x720.jpg"/></ul>
+                <ul>$10 - Mollusk Mash</ul>
+                <ul><img src="https://columbian.gwu.edu/sites/g/files/zaxdzs4901/files/image/Nautilus%20Pic_1080x720.jpg"/></ul>
+            </li>
+            <br/>
+            <h2>Entrees</h2>
+            <li>
+                <ul>$15 - Sea turtle soup</ul>
+                <ul><img src="https://www.vacationscostarica.com/_next/image/?url=https%3A%2F%2Fres.cloudinary.com%2Fvacationscostarica-com%2Fimage%2Fupload%2Fc_scale%2Cw_640%2Cq_auto%2Cf_webp%2Fgreen_sea_turtle_stock_diving_8223c87d38.jpg&w=992&q=75"/></ul>
+                <ul>$15 - Manatee Meatballs</ul>
+                <ul><img src="https://www.nhm.ac.uk/content/dam/nhm-www/discover/manatees/manatee-close-up-full-width.jpg.thumb.1160.1160.png"/></ul>
+                <ul>$25 - Smokey Honey Mustard Snail Chops</ul>
+                <ul><img src="https://columbian.gwu.edu/sites/g/files/zaxdzs4901/files/image/Nautilus%20Pic_1080x720.jpg"/></ul>
+                <ul>$25 - Creamy Pasta with Peppers and Smoked Zooxanthellae</ul>
+                <ul><img src="https://oceanservice.noaa.gov/education/tutorial_corals/coral-polyps.jpg"/></ul>
+            </li>
+            <br/>
+            <br/>
+            <br/>
+        </div>
+    )
+}
